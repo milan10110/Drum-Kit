fff98dc3279659e3cc71913a23b3f1b77789886c
Milan Vaghasiya <51287024+milan10110@users.noreply.github.com>
2022-07-12 22:27:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playsound);
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform')
+        return;
+    this.classList.remove('playing');
+}
+
+function playsound(e) {
+    const code = e.keyCode;
+    const audio = document.querySelector(`audio[data-key="${code}"]`);
+    const key = document.querySelector(`div[data-key="${code}"]`);
+
+    if(!audio){return};
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
